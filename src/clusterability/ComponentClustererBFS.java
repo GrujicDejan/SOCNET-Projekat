d3c2dcafc05bbb8afa3ccc8f21016ba2f15d741d
GrujicDejan <dejan.grujic2000@gmail.com>
2022-07-29 23:15:38 +0200

    update

--- src/clusterability/ComponentClustererBFS.java
+++ src/clusterability/ComponentClustererBFS.java
@@ -2,42 +2,48 @@ package clusterability;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
+import org.apache.commons.collections15.Transformer;
 import edu.uci.ics.jung.graph.UndirectedSparseGraph;
+import edu.uci.ics.jung.io.GraphMLWriter;
 import interfaces.ComponentClustererU;
+import model.edge.Mark;
+import model.edge.MarkedEdge;
 
 public class ComponentClustererBFS<V, E> implements ComponentClustererU<V, E> {
 
-	// input
-	private UndirectedSparseGraph<V, E> g;
-
-	// output
-	private List<UndirectedSparseGraph<V, E>> comps;
+	private UndirectedSparseGraph<V, E> graph;
+	private List<UndirectedSparseGraph<V, E>> components;
+	
+	private List<UndirectedSparseGraph<V, E>> clusterWithNegativeLink = new ArrayList<>();
+	List<MarkedEdge<E, V>> negativeEdges = new ArrayList<>();
+	
+	private Transformer<E, MarkedEdge<E, V>> edgeTransformer;
 
 	HashSet<V> visited = new HashSet<V>();
 
 	public ComponentClustererBFS(UndirectedSparseGraph<V, E> g) {
 		if (g == null || g.getVertexCount() == 0)
 			throw new IllegalArgumentException("Empty network");
-		this.g = g;
-		comps = new ArrayList<UndirectedSparseGraph<V, E>>();
+		this.graph = g;
+		components = new ArrayList<UndirectedSparseGraph<V, E>>();
 		identifyComponents();
 	}
 
 	private void identifyComponents() {
-		Iterator<V> it = g.getVertices().iterator();
+		Iterator<V> it = graph.getVertices().iterator();
+		
 		while (it.hasNext()) {
 			V node = it.next();
 			if (!visited.contains(node)) {
 				identifyComponent(node);
 			}
 		}
-		Collections.sort(comps, (c1, c2) -> c2.getVertexCount() - c1.getVertexCount());
+		
+		Collections.sort(components, (c1, c2) -> c2.getVertexCount() - c1.getVertexCount());
 	}
 
 	private void identifyComponent(V startNode) {
@@ -49,7 +55,8 @@ public class ComponentClustererBFS<V, E> implements ComponentClustererU<V, E> {
 		
 		while (!queue.isEmpty()) {
 			V current = queue.removeFirst();
-			Iterator<V> nit = g.getNeighbors(current).iterator();
+			
+			Iterator<V> nit = graph.getNeighbors(current).iterator();
 			while (nit.hasNext()) {
 				V neighbor = nit.next();
 				if (!visited.contains(neighbor)) {
@@ -58,23 +65,66 @@ public class ComponentClustererBFS<V, E> implements ComponentClustererU<V, E> {
 					component.addVertex(neighbor);
 				}
 				if (component.findEdge(current, neighbor) == null) {
-					E link = g.findEdge(current, neighbor);
+					E link = graph.findEdge(current, neighbor);
 					component.addEdge(link, current, neighbor);
 				}
 			}
 		}
-		comps.add(component);
+		identifyNegativeLinkInComponent(component);
+		components.add(component);
 	}
+	
+	private void identifyNegativeLinkInComponent(UndirectedSparseGraph<V, E> component) {
+		
+		boolean hasNegativeLink = false;
+		
+		List<V> nodes = new ArrayList<>(component.getVertices());
+		
+		Iterator<V> it1 = nodes.iterator();
+		Iterator<V> it2 = nodes.iterator();
+		
+		if (it2.hasNext()) {
+			it2.next();
+		}
+		
+		while (it2.hasNext()) {
+			V n1 = it1.next();
+			V n2 = it2.next();
+			
+			E link = this.graph.findEdge(n1, n2);
+			
+			if (link != null) {
+				MarkedEdge<E, V> edge = edgeTransformer.transform(link);
+				
+				if (edge.getMark() == Mark.NEGATIVE) {
+					if (component.findEdge(n1, n2) == null) {
+						component.addEdge(link, n1, n2);
+					}
+					if (!hasNegativeLink) {
+						clusterWithNegativeLink.add(component);
+					}
+					this.negativeEdges.add(new MarkedEdge<E, V>(link, n1, n2, Mark.NEGATIVE));
+					hasNegativeLink = true;
+				}
+				
+			}
+		}
+		
+	}
+	
+//	public void exportComponentToGraphML(String fileName) {
+//		
+//	}
 
 	// metode propisane interfejsom ComponentClustererU<V, E>
 	@Override
 	public List<UndirectedSparseGraph<V, E>> getComponents() {
-		return comps;
+		return components;
 	}
 	
 	@Override
 	public UndirectedSparseGraph<V, E> getGiantComponent() {
-		return comps.get(0);
+		return components.get(0);
 	}
 
 }
--- src/model/node/Node.java
+++ src/model/node/Node.java
@@ -3,13 +3,19 @@ package model.node;
 public class Node {
 
 	private String label;
+	private String id;
 
-	public Node(String label) {
+	public Node(String label, String id) {
 		this.label = label;
+		this.id = id;
 	}
 
 	public String getLabel() {
 		return label;
+	}
+	
+	public String getId() {
+		return id;
 	}
 
 }
